bd1b2507ae02e442b9018431b41be43a3f5a426d
QianhengZhang <59622094+QianhengZhang@users.noreply.github.com>
2022-05-25 12:22:26 -0700

    push new hero

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import pygame
-from model import Knight
+from model import Hero
 
 def main():
     global avatar
     pygame.init()
-    window_size_x = 1200
-    window_size_y = 950
+    window_size_x = 1050
+    window_size_y = 700
     surface = pygame.display.set_mode([window_size_x,window_size_y])
-    pygame.display.set_caption('Arena Game')
+    pygame.display.set_caption('Game')
 
-    background = pygame.image.load('assets/imgs/background/BG.png').convert()
-
-    avatar = Knight((100, 700))
+    background = pygame.image.load('Run.png').convert()
+    background = pygame. transform. scale(background, (window_size_x, window_size_y))
+    avatar = Hero((60, 350))
     clock = pygame.time.Clock()
 
     while True:
-        clock.tick(60)
+        clock.tick(21)
 
-        quit, left, right, click = check_events()
-        if quit:
+        controls = check_events()
+        if controls['quit']:
             break
-
-        avatar.update(click, left, right)
-
         surface.blit(background, (0,0))
+        avatar.update(controls)
         avatar.draw(surface)
         pygame.display.flip()
 
@@ -31,37 +29,62 @@ def main():
 def check_events():
     ''' A controller of sorts.  Looks for Quit, arrow type events.  Space initiates a jump.
     '''
-    quit = False
-    click = None
-    left = False
-    right = False
+    controls = {
+        'quit' : False,
+        'left' : False,
+        'right' : False,
+        'attack': False,
+        'block': False,
+        'up': False,
+        'down': False
+    }
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            quit = True
+            controls['quit'] = True
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                quit = True
+                controls['quit'] = True
             if event.key == pygame.K_ESCAPE:
-                quit = True
-            if event.key == pygame.K_a:
-                left = True
-            if event.key == pygame.K_d:
-                right = True
-
+                controls['quit'] = True
+            if event.key == pygame.K_SPACE:
+                controls['jump'] = True
+            if event.key == pygame.K_j:
+                controls['attack'] = True
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                left = False
-            if event.key == pygame.K_d:
-                right = False
-        if event.type == pygame.MOUSEBUTTONUP:
-                click = event.pos
-    if right and left:
-        right = False
-        left = False
+            pass
+    key_pressed = pygame.key.get_pressed()
+    if key_pressed[pygame.K_a]:
+        controls['left'] = True
+    else:
+        controls['left'] = False
+    if key_pressed[pygame.K_d]:
+        controls['right'] = True
+    else:
+        controls['right'] = False
+    if key_pressed[pygame.K_w]:
+        controls['up'] = True
+    else:
+        controls['up'] = False
+    if key_pressed[pygame.K_s]:
+        controls['down'] = True
+    else:
+        controls['down'] = False
 
-    return (quit, left, right, click)
+    if key_pressed[pygame.K_k]:
+        controls['block'] = True
+        controls['left'] = False
+        controls['right'] = False
+        controls['up'] = False
+        controls['down'] = False
+    if controls['left'] and controls['right']:
+        controls['left'] = False
+        controls['right'] = False
+    if controls['up'] and controls['down']:
+        controls['up'] = False
+        controls['down'] = False
+    return controls
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,23 +1,78 @@
+from turtle import update
 import pygame
+import time
+global HERO_ASSET
 
-class Knight(pygame.sprite.Sprite):
+HERO_ASSET = 'assets/imgs/Sprites/HeroKnight/'
+
+
+class Hero(pygame.sprite.Sprite):
 
     def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
+        self.maxHp = 120
+        self.hp = 120
+        self.attack = 20
+        self.coolDown = 0.7
+        self.status = 'idle'
+        self.direction = 1
+        self.velocity = 12
+        self.mass = 2
+        self.index = 0
+        self.last = time.time()
+        self.images = {
+            'idle': [pygame.image.load(HERO_ASSET + f'idle/HeroKnight_Idle_{i+1}.png') for i in range(0, 7)],
+            'run': [pygame.image.load(HERO_ASSET + f'run/HeroKnight_Run_{i+1}.png') for i in range(0, 9)],
+            'jump': [pygame.image.load(HERO_ASSET + f'Jump/HeroKnight_Jump_{i+1}.png') for i in range(0, 5)],
+            'fall': [pygame.image.load(HERO_ASSET + f'fall/HeroKnight_fall_{i+1}.png') for i in range(0, 3)],
+            'attack' : [pygame.image.load(HERO_ASSET + f'Attack1/HeroKnight_Attack1_{i+1}.png') for i in range(0, 5)],
+            'attack2' : [pygame.image.load(HERO_ASSET + f'Attack2/HeroKnight_Attack2_{i+1}.png') for i in range(0, 5)],
+            'attack3' : [pygame.image.load(HERO_ASSET + f'Attack3/HeroKnight_Attack3_{i+1}.png') for i in range(0, 7)],
 
-        self.image = pygame.Surface((100,120))
-        image_surf = pygame.image.load('assets/imgs/knight/png/Idle.png').convert()
+            'block' : [pygame.image.load(HERO_ASSET + f'BlockIdle/HeroKnight_Block Idle_{i+1}.png') for i in range(0, 7)]
+        }
+
+        image_surf = pygame.image.load(HERO_ASSET + 'idle/HeroKnight_Idle_0.png').convert()
+        self.image = pygame.Surface((100,55))
         self.image.blit(image_surf, (0,0))
         self.image.set_colorkey((0,0,0))
         self.rect = self.image.get_rect(topleft=pos)
 
-    def update(self, click, left, right):
-        if click:
-            self.rect.center = click
-        if left:
-            self.rect.x -= 15
-        if right:
-            self.rect.x += 15
+    def update(self, controls):
+        new = time.time()
+        if controls['attack'] and (new-self.last > self.coolDown):
+            if controls['block']:
+                self.status = 'attack2'
+            elif self.status == 'run':
+                self.status = 'attack3'
+            else:
+                self.status = 'attack'
+            self.index = 0
+            self.last = time.time()
+        elif controls['up']:
+            self.status = 'run'
+            self.rect.y -= self.velocity
+        elif controls['down']:
+            self.status = 'run'
+            self.rect.y += self.velocity
+        elif controls['left']:
+            self.direction = -1
+            self.status = 'run'
+            self.rect.x -= self.velocity
+        elif controls['right']:
+            self.direction = 1
+            self.status = 'run'
+            self.rect.x += self.velocity
+        elif controls['block']:
+            self.status = 'block'
+            self.index = 0
+        else:
+            self.status = 'idle'
+        self.index = (self.index + 1) % len(self.images[self.status])
+        self.image = self.images[self.status][self.index]
+        if self.direction == -1:
+            self.image = pygame.transform.flip(self.image, True, False)
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+
